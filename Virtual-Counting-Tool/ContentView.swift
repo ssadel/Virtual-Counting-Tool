929203a659f47b95602393427de29e0e9f7be378
Sidney Sadel <tuj60668@temple.edu>
2022-02-09 12:04:55 -0500

    Initial Commit

---
+++ Virtual-Counting-Tool/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Virtual-Counting-Tool
+//
+//  Created by Sidney Sadel on 2/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Virtual-Counting-Tool/Virtual_Counting_ToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Virtual_Counting_ToolApp.swift
+//  Virtual-Counting-Tool
+//
+//  Created by Sidney Sadel on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Virtual_Counting_ToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
